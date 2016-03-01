4e835360293142eff6faa38a2730c5487ee3bc0a
Raphael Koehler-Derrick <koehr998@newschool.edu>
2016-03-01 18:29:20 -0500

    Weirdness resolved

--- sketch.js
+++ sketch.js
@@ -3,16 +3,52 @@
 //========================
 
 //your sun/moon object:
+var Circle = {
+	x: 500,
+	y: 300,
+	d: 500,
+	c: "#ffcc66"
+};
+
 
 //your building object:
+
+var Buildings = [];
+var Buildings2 = [];
+
+
 
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 
+	for(var i = 0; i < 30 ;i = i+1){
+ 		Buildings.push( new Build(i*random(30,100),height/2) );
+
+
+	}
 }
 
 function draw(){
 
+	noStroke();
+	fill(Circle.c);
+	ellipse(Circle.x, Circle.y,Circle.d, Circle.d);
+
+	for(var i = 0; i < Buildings.length; i++){
+		Buildings[i].drawBuilding();
+
+	}
+
 
 }
+
+function Build(x,y){
+	this.x = x;
+	this.y = y;
+	this.drawBuilding = function(){
+		noStroke();
+		fill(50);
+		rect(this.x, this.y,25, 100);
+	}
+};
